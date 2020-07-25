feed3dd023b65e7ef83872f02a8cb97555e3c8af
poria1372 <poria.karimi1372@gmail.com>
2020-07-25 23:11:04 +0430

    Update Main.java

--- src/main/java/PACKAGE_NAME/Main.java
+++ src/main/java/PACKAGE_NAME/Main.java
@@ -2,7 +2,6 @@ package PACKAGE_NAME;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello java");
+        System.out.println("hello javaa");
     }
 }

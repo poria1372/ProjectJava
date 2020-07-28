196ccc70e1833dcff4cc4b26ed28edfd985919d5
poria1372 <poria.karimi1372@gmail.com>
2020-07-28 13:27:08 +0430

    Update Main.java

--- src/main/java/com/examples/Main.java
+++ src/main/java/com/examples/Main.java
@@ -1,7 +1,28 @@
 package com.examples;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello javaa");
+    public static void main(String[] args) throws Exception {
+        writeFileChannel(ByteBuffer.wrap("debtor_depositNumber_amount \r pouria_639882688_1000000  ".getBytes()));
+
+    }
+    public static void writeFileChannel(ByteBuffer byteBuffer)throws Exception {
+        Set<StandardOpenOption> options = new HashSet<>();
+        options.add(StandardOpenOption.CREATE);
+        options.add(StandardOpenOption.APPEND);
+        Path path = Paths.get("c:/java/person.txt");
+        FileChannel fileChannel = FileChannel.open(path, options);
+        fileChannel.write(byteBuffer);
+        fileChannel.close();
     }
 }
+
+

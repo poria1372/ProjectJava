0e641969d450edfc087d0956dac5f57fbf7e0c81
poria1372 <poria.karimi1372@gmail.com>
2020-07-29 16:32:24 +0430

    Update Main.java

--- src/main/java/com/examples/Main.java
+++ src/main/java/com/examples/Main.java
@@ -3,25 +3,27 @@ package com.examples;
 
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        writeFileChannel(ByteBuffer.wrap(" PouriaKarimi 639882688 1000000 \n MeysamKarimi 584712698 300000 \n AmirHatami 6398258441 500000 ".getBytes()));
 
 
-    }
-    public static void writeFileChannel(ByteBuffer byteBuffer)throws Exception {
-        Set<StandardOpenOption> options = new HashSet<>();
-        options.add(StandardOpenOption.CREATE);
-        options.add(StandardOpenOption.APPEND);
-        Path path = Paths.get("c:/java/Debtor.txt");
-        FileChannel fileChannel = FileChannel.open(path, options);
-        fileChannel.write(byteBuffer);
-        fileChannel.close();
+        Path path= Paths.get("c:\\java\\Debtor.txt");
+       Path done= Files.createFile(path);
+
+            String content="pouriaKArimi 639852147 500000 \n MeysamKarimi 369852147 3000000";
+
+            Files.write(path,content.getBytes(), StandardOpenOption.APPEND);
+
+
+        System.out.println("file created ");
+
     }
 }
